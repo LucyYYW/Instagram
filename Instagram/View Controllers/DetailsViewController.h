c8c2a81516bdd468205eed1b5b1178c7ddff36ec
Yuewei Yuan <lucyyyw@fb.com>
2019-07-09 23:38:54 -0700

    creating outlets for the details view controller

--- Instagram/View Controllers/DetailsViewController.h
+++ Instagram/View Controllers/DetailsViewController.h
@@ -11,6 +11,14 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface DetailsViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *profileImageView;
+@property (weak, nonatomic) IBOutlet UILabel *userNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *postImageView;
+@property (weak, nonatomic) IBOutlet UILabel *captionLabel;
+@property (weak, nonatomic) IBOutlet UILabel *likeLabel;
+@property (weak, nonatomic) IBOutlet UIButton *likeButton;
+@property (weak, nonatomic) IBOutlet UIButton *commentButton;
 
 @end
 
