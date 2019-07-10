5f792ef8f18515fc43eb822781c5fa7303d6985e
Yuewei Yuan <lucyyyw@fb.com>
2019-07-10 15:14:51 -0700

    (add an column to the Parse server for the user profileImage) setting up the my profile view controller + creating an edit profile view controller

---
+++ Instagram/View Controllers/MyProfileViewController.m
@@ -0,0 +1,34 @@
+//
+//  MyProfileViewController.m
+//  Instagram
+//
+//  Created by lucyyyw on 7/10/19.
+//  Copyright © 2019 lucyyyw. All rights reserved.
+//
+
+#import "MyProfileViewController.h"
+
+@interface MyProfileViewController ()
+
+@end
+
+@implementation MyProfileViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+- (IBAction)onEditProfile:(id)sender {
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
